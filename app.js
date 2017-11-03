7a3cd8f054f598bef3587c4ef0ceda4b8b9150b4
qinqiang <qinqiang@v1.cn>
2017-11-03 13:56:22 +0800

    second

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
+    // 展示本地存储能力   
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
